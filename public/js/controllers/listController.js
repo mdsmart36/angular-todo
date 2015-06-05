4a19e3fc0502513866b3b518bbcd0b1604cf777f
Matthew Smart <mdsmart36@gmail.com>
2015-06-05 13:57:30 -0500

    remove and edit function

--- public/js/controllers/listController.js
+++ public/js/controllers/listController.js
@@ -4,7 +4,7 @@ controllers.controller('ListCtrl', function ($scope, $http) {
 
     $http({
           method: 'GET',
-          url: '/api/tasks',
+          url: '/api/tasks'
         }).
         success(function (data, status, headers, config) {
           console.log(data);
@@ -13,5 +13,23 @@ controllers.controller('ListCtrl', function ($scope, $http) {
         error(function (data, status, headers, config) {
           console.log('could not get the tasks');
         });
+
+    $scope.edit = function() {
+
+    }
+
+    $scope.remove = function(id) {
+      $http({
+        method: 'DELETE',
+        url: '/api/tasks/' + id
+      }).
+      success(function (data, status, headers, config){
+        console.log('item deleted');
+        // refresh the page?
+      }).
+      error(function (data, status, headers, config){
+        console.log('item not deleted');
+      });
+    }
 
 });
